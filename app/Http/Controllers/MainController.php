6f28857d82bd9a2cedb469fe4074ac797ebed9b7
masinc <masinc000@gmail.com>
2018-03-02 10:47:59 +0900

    Json読み込み機構を削除

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,15 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Storage;
 
-use App\Models\JsonCard;
-
 class MainController extends Controller
 {
     //
     public function index(Request $request){
-        
-        $jsonCard = JsonCard::get();
-        $items = $jsonCard->items();
 
         // $data = ['items' => $items];
         $data = ['items' => []];
