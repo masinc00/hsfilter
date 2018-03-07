c0e5db3311380f1ca0b35dec3208c28ec5ff0f02
masinc <masinc000@gmail.com>
2018-03-07 14:18:36 +0900

    jsonをsqliteに変換する処理がきちんと動作しなかったのを修正 大幅なリファクタリング

--- app/Http/Controllers/Json2SqlConvertController.php
+++ app/Http/Controllers/Json2SqlConvertController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\JsonConverter;
-
+use App\Models\Json2SqlConverter;
+use Illuminate\Filesystem\Filesystem;
 
 class Json2SqlConvertController extends Controller
 {
     public function index(Request $request){
-
+        $fs = new FileSystem();
+        $file = $fs->get(public_path("storage/cards.json"));
+        $json = json_decode($file);
+        Json2SqlConverter::convert($json);
     }
 }
--- app/Models/Json2SqlConverter.php
+++ app/Models/Json2SqlConverter.php
@@ -16,11 +16,15 @@ class Json2SqlConverter extends Model
      * $json jsonオブジェクト
      * $table_name テーブル名を指定する
      */
-    public function __construct($json, $table_name = "cards"){
+    private function __construct($json, $table_name = "cards"){
         $this->json = $json;
         $this->table_name = $table_name;
         
-        DB::transaction($this->connect);
+        DB::transaction(function () { $this->connect(); });
+    }
+
+    public static function convert($json, $table_name = "cards"){
+        new Json2SqlConverter($json, $table_name);
     }
 
     //マルチ言語系の定数
@@ -29,103 +33,129 @@ class Json2SqlConverter extends Model
     const text = "text";
     const flavor = "flavor";
 
-    //sqlに書き込む
+    //sqlに書き込む処理
     private function connect(){
-        $f_artist = ":artist";
-        $f_attack = ":attack";
-        $f_cardClass = ":cardClass";
-        $f_collectible = ":collectible";
-        $f_cost = ":cost";
-        $f_dbfId = ":dbfId";
-        $f_health = ":health";
-        $f_id = ":id";
-        $f_playRequirements = ":playRequirements";
-        $f_playerClass = ":playerClass";
-        $f_rarity = ":rarity";
-        $f_set = ":set";
-        $f_type = ":type";
+        //$table_name = $this->table_name;
 
-
-        //テーブルを作成する
-        function create_table(){
-            //マルチ言語対応のフィールドのための定義を作成する
-            function make_multilang_defines($param_name){
-                $results = array_map(function ($x) use ($param_name) {
-                    return "${param_name}_${x} TEXT";
-                }, langs);
-            
-                return join("," , $results);
-            } 
-            $corumn_sql = [
-                "artist TEXT",
-                "attack INTEGER",
-                "cardClass TEXT",
-                "collectible INTEGER",
-                "cost INTEGER",
-                "dbfId INTEGER",
-                "health INTEGER",
-                "id TEXT primary key",
-                "playRequirements TEXT",
-                "playerClass TEXT",
-                "rarity TEXT",
-                "'set' TEXT",
-                "type TEXT",
-            ];
-
-            $sql_create_table = "create table if not exists". $this->table_name . "("
-            . join(",", $corumn_sql) . ","
-            . make_multilang_defines("name") . ", "
-            . make_multilang_defines("text") . ", "
-            . make_multilang_defines("flavor")
-            . ")";
-
-            DB::statement($sql_create_table);
+        $this->create_table();
+        foreach($this->json as $item){
+            $this->insert($item);
         }
+    }
 
-        //データを挿入する
-        function insert($item){
-            //マルチ言語対応フィールドのためのカラムフラグを作成する
-            function make_multilang_flags($param_name){
-                $results = array_map(function ($x) use ($param_name) {
-                    return ":${param_name}_${x}";
-                }, langs);
-            
-                return join("," , $results);
-            }
-            $params = [
-                $f_artist       => isset($item->artist)     ? $item->artist     : "",
-                $f_attack       => isset($item->attack)     ? $item->attack     : "",
-                $f_cardClass    => isset($item->cardClass)  ? $item->cardClass  : "",
-                $f_collectible  => isset($item->collectible)? $item->collectible: "",
-                $f_cost         => isset($item->cost)       ? $item->cost       : "",
-                $f_dbfId        => isset($item->dbfId)      ? $item->dbfId      : "",
-                $f_health       => isset($item->health)     ? $item->health     : "",
-                $f_id           => isset($item->id)         ? $item->id         : "",
-                $f_playRequirements => isset($item->playRequirements) ? json_encode($item->playRequirements) : "",
-                $f_playerClass  => isset($item->playerClass)? $item->playerClass: "",
-                $f_rarity       => isset($item->rarity)     ? $item->rarity     : "",
-                $f_set          => isset($item->set)        ? $item->set        : "",
-                $f_type         => isset($item->type)       ? $item->type       : ""
-            ];   
-            $params += make_multilang_params(name,   isset($item->name)   ? $item->name   : [] );
-            $params += make_multilang_params(flavor, isset($item->flavor) ? $item->flavor : [] );
-            $params += make_multilang_params(text,   isset($item->text)   ? $item->text : [] );
-            
-            $sql_insert = "insert into". $this->$this->table_name  ."values (
-                ${f_artist} , ${f_attack}, ${f_cardClass}, ${f_collectible}, ${f_cost}, ${f_dbfId},
-                ${f_health}, ${f_id}, ${f_playRequirements}, ${f_playerClass}, ${f_rarity}, ${f_set}, ${f_type},"
-                . make_multilang_flags("name") . ", "
-                . make_multilang_flags("text") . ", "
-                . make_multilang_flags("flavor") .
-                ")";
-            
-            DB::insert($sql_insert, $params);
 
-        }
+    //テーブルを作成する
+    function create_table() {
+        //マルチ言語対応のフィールドのための定義を作成する
+        function make_multilang_defines($param_name){
+            $results = array_map(function ($x) use ($param_name) {
+                return "${param_name}_${x} TEXT";
+            }, Json2SqlConverter::langs);
+        
+            return join("," , $results);
+        } 
+        $corumn_sql = [
+            "artist TEXT",
+            "attack INTEGER",
+            "cardClass TEXT",
+            "collectible INTEGER",
+            "cost INTEGER",
+            "dbfId INTEGER",
+            "health INTEGER",
+            "id TEXT primary key",
+            "playRequirements TEXT",
+            "playerClass TEXT",
+            "rarity TEXT",
+            "'set' TEXT",
+            "type TEXT",
+        ];        
+        // $table_name = $this->table_name;
+        $sql_create_table = "create table if not exists ". $this->table_name . "("
+        . join(",", $corumn_sql) . ","
+        . make_multilang_defines("name") . ", "
+        . make_multilang_defines("text") . ", "
+        . make_multilang_defines("flavor")
+        . ")";
 
-        create_table();
-        foreach($json as $item){
-            insert($item);
-        }
+        DB::statement($sql_create_table);
+    }
+
+    //マルチ言語対応フィールドのためのカラムフラグを作成する
+    function make_multilang_flags($param_name){
+        $results = array_map(function ($x) use ($param_name) {
+            return ":${param_name}_${x}";
+        }, Json2SqlConverter::langs);
+    
+        return join("," , $results);
+    }
+
+    //多言語用のパラメータを作成する
+    function make_multilang_params($param_name, $params){
+        //$result = [];
+        $result["${param_name}_enUS"] = isset($params->enUS) ? $params->enUS : ""; 
+        $result["${param_name}_jaJP"] = isset($params->jaJP) ? $params->jaJP : "";
+        $result["${param_name}_deDE"] = isset($params->deDE) ? $params->deDE : "";
+        $result["${param_name}_esES"] = isset($params->esES) ? $params->esES : "";
+        $result["${param_name}_esMX"] = isset($params->esMX) ? $params->esMX : "";
+        $result["${param_name}_frFR"] = isset($params->frFR) ? $params->frFR : "";
+        $result["${param_name}_itIT"] = isset($params->itIT) ? $params->itIT : "";
+        $result["${param_name}_koKR"] = isset($params->koKR) ? $params->koKR : "";
+        $result["${param_name}_plPL"] = isset($params->plPL) ? $params->plPL : "";
+        $result["${param_name}_ptBR"] = isset($params->ptBR) ? $params->ptBR : "";
+        $result["${param_name}_ruRU"] = isset($params->ruRU) ? $params->ruRU : "";
+        $result["${param_name}_thTH"] = isset($params->thTH) ? $params->thTH : "";
+        $result["${param_name}_zhCN"] = isset($params->zhCN) ? $params->zhCN : "";
+        $result["${param_name}_zhTW"] = isset($params->zhTW) ? $params->zhTW : "";
+        return array_filter($result);
+    }
+
+    //insert時のフィールドパラメーター
+    const f_artist = ":artist";
+    const f_attack = ":attack";
+    const f_cardClass = ":cardClass";
+    const f_collectible = ":collectible";
+    const f_cost = ":cost";
+    const f_dbfId = ":dbfId";
+    const f_health = ":health";
+    const f_id = ":id";
+    const f_playRequirements = ":playRequirements";
+    const f_playerClass = ":playerClass";
+    const f_rarity = ":rarity";
+    const f_set = ":set";
+    const f_type = ":type";
+
+    const base_fields = [self::f_artist, self::f_attack, self::f_cardClass, self::f_collectible, self::f_cost, self::f_dbfId, 
+            self::f_health, self::f_id, self::f_playRequirements, self::f_playerClass, self::f_rarity, self::f_set, self::f_type];
+
+    //個別のデータを挿入する
+    function insert($item){
+        $params = [
+            self::f_artist       => isset($item->artist)     ? $item->artist     : "",
+            self::f_attack       => isset($item->attack)     ? $item->attack     : "",
+            self::f_cardClass    => isset($item->cardClass)  ? $item->cardClass  : "",
+            self::f_collectible  => isset($item->collectible)? $item->collectible: "",
+            self::f_cost         => isset($item->cost)       ? $item->cost       : "",
+            self::f_dbfId        => isset($item->dbfId)      ? $item->dbfId      : "",
+            self::f_health       => isset($item->health)     ? $item->health     : "",
+            self::f_id           => isset($item->id)         ? $item->id         : "",
+            self::f_playRequirements => isset($item->playRequirements) ? json_encode($item->playRequirements) : "",
+            self::f_playerClass  => isset($item->playerClass)? $item->playerClass: "",
+            self::f_rarity       => isset($item->rarity)     ? $item->rarity     : "",
+            self::f_set          => isset($item->set)        ? $item->set        : "",
+            self::f_type         => isset($item->type)       ? $item->type       : ""
+        ];   
+        $params += $this->make_multilang_params(Json2SqlConverter::name,   isset($item->name)   ? $item->name   : [] );
+        $params += $this->make_multilang_params(Json2SqlConverter::flavor, isset($item->flavor) ? $item->flavor : [] );
+        $params += $this->make_multilang_params(Json2SqlConverter::text,   isset($item->text)   ? $item->text : [] );
+        
+        $sql_insert = "insert into ". ($this->table_name)  ." values(" .
+            join(",", self::base_fields ) . ","
+            . $this->make_multilang_flags("name") . ", "
+            . $this->make_multilang_flags("text") . ", "
+            . $this->make_multilang_flags("flavor") .
+            ")";
+        
+        DB::insert($sql_insert, $params);
+
     }
 }
